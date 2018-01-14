5aba3019e4a39f43348177d29031ee2df16ca5c5
Karthik Arumugam <chivalrykarthik@gmail.com>
2018-01-14 13:00:33 +0530

    sample

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer((req,res)=>{
     res.writeHead(200,{"content-type":"text/pain"});
-    res.end("hello world sample program");
+    res.end("hello world sample program github");
 }).listen(process.env.PORT || 3000);
